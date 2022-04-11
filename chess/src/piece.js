48f678f889e872dbfae7c1dbad1a68837dc064d7
AndrewMitchell25 <andrew.j.mitchell.247@gmail.com>
2022-04-10 21:49:57 -0400

    Started working on classes

--- chess/src/piece.js
+++ chess/src/piece.js
@@ -1,5 +1,20 @@
-class Piece{
-    constructor(x, y, isWhite){
+class Piece extends React.Component{
+    constructor(x, y, isWhite, img){
         this.white = isWhite;
+        this.value = 0;
+        this.isMoving = false;
+        this.img = img;
+    }
+}
+
+class King extends Piece{
+    constructor(x, y, isWhite){
+        super(x, y, isWhite);
+        this.value = 100;
+        if(isWhite){
+            this.img = "whiteKing";
+        } else {
+            this.img = "blackKing";
+        }
     }
 }
