5b62bffe9afe4ce9aacc2b44a3baa35a09629eb2
AndrewMitchell25 <andrew.j.mitchell.247@gmail.com>
2022-08-26 14:15:49 -0400

    Board setup

--- src/components/board.js
+++ src/components/board.js
@@ -6,7 +6,6 @@ import Rook from "../pieces/rook";
 import Knight from "../pieces/knight";
 import Bishop from "../pieces/bishop";
 import Pawn from "../pieces/pawn";
-import Piece from "../pieces/piece";
 
 export default class Board extends React.Component {
     constructor() {
@@ -17,46 +16,42 @@ export default class Board extends React.Component {
             board : []
         }
 
-        //this.state.pieces.push(<Rook id={0} x={0} y={0} isWhite={false}/>)
-        this.state.pieces.push({type: "Rook", id: 0, isWhite: true, style: ""})
-        this.state.pieces.push({type: "King", id: 29, isWhite: true, style: ""})
-        this.state.pieces.push({type: "Queen", id: 32, isWhite: false, style: ""})
-        this.state.pieces.push({type: "Pawn", id: 19, isWhite: false, style: ""})
-        /*
-        this.pieces.push(new Knight(0, 1, false))
-        this.pieces.push(new Bishop(0, 2, false))
-        this.pieces.push(new Queen(0, 3, false))
-        this.pieces.push(new King(0, 4, false))
-        this.pieces.push(new Bishop(0, 5, false))
-        this.pieces.push(new Knight(0, 6, false))
-        this.pieces.push(new Rook(0, 7, false))
-        this.pieces.push(new Pawn(1, 0, false))
-        this.pieces.push(new Pawn(1, 1, false))
-        this.pieces.push(new Pawn(1, 2, false))
-        this.pieces.push(new Pawn(1, 3, false))
-        this.pieces.push(new Pawn(1, 4, false))
-        this.pieces.push(new Pawn(1, 5, false))
-        this.pieces.push(new Pawn(1, 6, false))
-        this.pieces.push(new Pawn(1, 7, false))
+        this.state.pieces.push({type: "Rook", x: 0, y: 0, isWhite: false, style: ""})
+        this.state.pieces.push({type: "Knight", x: 0, y: 1, isWhite: false, style: ""})
+        this.state.pieces.push({type: "Bishop", x: 0, y: 2, isWhite: false, style: ""})
+        this.state.pieces.push({type: "Queen", x: 0, y: 3, isWhite: false, style: ""})
+        this.state.pieces.push({type: "King", x: 0, y: 4, isWhite: false, style: ""})
+        this.state.pieces.push({type: "Bishop", x: 0, y: 5, isWhite: false, style: ""})
+        this.state.pieces.push({type: "Knight", x: 0, y: 6, isWhite: false, style: ""})
+        this.state.pieces.push({type: "Rook", x: 0, y: 7, isWhite: false, style: ""})
 
-        this.pieces.push(new Pawn(6, 0, true))
-        this.pieces.push(new Pawn(6, 1, true))
-        this.pieces.push(new Pawn(6, 2, true))
-        this.pieces.push(new Pawn(6, 3, true))
-        this.pieces.push(new Pawn(6, 4, true))
-        this.pieces.push(new Pawn(6, 5, true))
-        this.pieces.push(new Pawn(6, 6, true))
-        this.pieces.push(new Pawn(6, 7, true))
-        this.pieces.push(new Rook(7, 0, true))
-        this.pieces.push(new Knight(7, 1, true))
-        this.pieces.push(new Bishop(7, 2, true))
-        this.pieces.push(new Queen(7, 3, true))
-        this.pieces.push(new King(7, 4, true))
-        this.pieces.push(new Bishop(7, 5, true))
-        this.pieces.push(new Knight(7, 6, true))
-        this.pieces.push(new Rook(7, 7, true))
-        this.pieces.push(new Piece(5,5, true))
-        */
+        this.state.pieces.push({type: "Pawn", x: 1, y: 0, isWhite: false, style: ""})
+        this.state.pieces.push({type: "Pawn", x: 1, y: 1, isWhite: false, style: ""})
+        this.state.pieces.push({type: "Pawn", x: 1, y: 2, isWhite: false, style: ""})
+        this.state.pieces.push({type: "Pawn", x: 1, y: 3, isWhite: false, style: ""})
+        this.state.pieces.push({type: "Pawn", x: 1, y: 4, isWhite: false, style: ""})
+        this.state.pieces.push({type: "Pawn", x: 1, y: 5, isWhite: false, style: ""})
+        this.state.pieces.push({type: "Pawn", x: 1, y: 6, isWhite: false, style: ""})
+        this.state.pieces.push({type: "Pawn", x: 1, y: 7, isWhite: false, style: ""})
+
+        this.state.pieces.push({type: "Pawn", x: 6, y: 0, isWhite: true, style: ""})
+        this.state.pieces.push({type: "Pawn", x: 6, y: 1, isWhite: true, style: ""})
+        this.state.pieces.push({type: "Pawn", x: 6, y: 2, isWhite: true, style: ""})
+        this.state.pieces.push({type: "Pawn", x: 6, y: 3, isWhite: true, style: ""})
+        this.state.pieces.push({type: "Pawn", x: 6, y: 4, isWhite: true, style: ""})
+        this.state.pieces.push({type: "Pawn", x: 6, y: 5, isWhite: true, style: ""})
+        this.state.pieces.push({type: "Pawn", x: 6, y: 6, isWhite: true, style: ""})
+        this.state.pieces.push({type: "Pawn", x: 6, y: 7, isWhite: true, style: ""})
+
+        this.state.pieces.push({type: "Rook", x: 7, y: 0, isWhite: true, style: ""})
+        this.state.pieces.push({type: "Knight", x: 7, y: 1, isWhite: true, style: ""})
+        this.state.pieces.push({type: "Bishop", x: 7, y: 2, isWhite: true, style: ""})
+        this.state.pieces.push({type: "Queen", x: 7, y: 3, isWhite: true, style: ""})
+        this.state.pieces.push({type: "King", x: 7, y: 4, isWhite: true, style: ""})
+        this.state.pieces.push({type: "Bishop", x: 7, y: 5, isWhite: true, style: ""})
+        this.state.pieces.push({type: "Knight", x: 7, y: 6, isWhite: true, style: ""})
+        this.state.pieces.push({type: "Rook", x: 7, y: 7, isWhite: true, style: ""})
+
 
         for(let i = 0; i < 8; i++){
             const row = [];
@@ -66,7 +61,7 @@ export default class Board extends React.Component {
                     color = "light";
                 }
                 //let square = <Square x={i} y={j} id={i*8+j} color={color} key={i*8+j} pieces={this.state.pieces}/>;
-                let square = {id: i*8+j, color: color, style: null}
+                let square = {id:i*8+j, color: color, style: null}
                 this.state.squares.push(square);
                 row.push(square);
             }
@@ -79,19 +74,20 @@ export default class Board extends React.Component {
             <Square key={id} 
                 style={style} 
                 color={color} 
-                piece={this.state.pieces.find(piece => piece.id === id)} 
+                piece={this.state.pieces.find(piece => piece.x*8 + piece.y === id)} 
                 renderPiece={this.renderPiece} 
                 />
         );
     }
 
-    renderPiece(type, id, style, isWhite, handlePieceClick){
+    renderPiece(type, x, y, id, style, isWhite){
         
         if(type === "King") {
             return(
                 <King key={id} 
                 style={style} 
-                id={id} 
+                x={x}
+                y={y} 
                 isWhite={isWhite}
                 />
             );
@@ -99,7 +95,8 @@ export default class Board extends React.Component {
             return(
                 <Queen key={id} 
                 style={style} 
-                id={id} 
+                x={x}
+                y={y}  
                 isWhite={isWhite}
                 />
             );
@@ -107,7 +104,8 @@ export default class Board extends React.Component {
             return(
                 <Rook key={id} 
                 style={style} 
-                id={id} 
+                x={x}
+                y={y}  
                 isWhite={isWhite}
                 />
             );
@@ -115,7 +113,8 @@ export default class Board extends React.Component {
             return(
                 <Bishop key={id} 
                 style={style} 
-                id={id} 
+                x={x}
+                y={y}  
                 isWhite={isWhite}
                 />
             );
@@ -123,7 +122,8 @@ export default class Board extends React.Component {
             return(
                 <Knight key={id} 
                 style={style} 
-                id={id} 
+                x={x}
+                y={y} 
                 isWhite={isWhite}
                 />
             );
@@ -131,7 +131,8 @@ export default class Board extends React.Component {
             return(
                 <Pawn key={id} 
                 style={style} 
-                id={id} 
+                x={x}
+                y={y} 
                 isWhite={isWhite}
                 />
             );
--- src/components/square.js
+++ src/components/square.js
@@ -2,11 +2,10 @@ import React from "react";
 
 export default class Square extends React.Component {
     render(){
-
         return(
             <div className={"square " + this.props.color + "-square"}
                 style={this.props.style}>
-                    {this.props.piece !== undefined ? this.props.renderPiece(this.props.piece.type, this.props.piece.id, this.props.piece.style, this.props.piece.isWhite, this.props.handlePieceClick) : null}
+                    {this.props.piece !== undefined ? this.props.renderPiece(this.props.piece.type, this.props.piece.x, this.props.piece.y, this.props.piece.x*8 + this.props.piece.y, this.props.piece.style, this.props.piece.isWhite, this.props.handlePieceClick) : null}
             </div>
         )
     }
